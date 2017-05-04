d121699406b2a4a3b9bc5859265a5c1c061ff300
demirramazan <rdemir4434@gmail.com>
2017-05-04 19:06:26 +0300

    jsondoc spring boot

--- src/main/java/com/rdemir/booking/controller/HotelControllerRepoSample.java
+++ src/main/java/com/rdemir/booking/controller/HotelControllerRepoSample.java
@@ -2,6 +2,10 @@ package com.rdemir.booking.controller;
 
 import com.rdemir.booking.model.HotelBooking;
 import com.rdemir.booking.reporsitory.HotelBookingRepository;
+import org.jsondoc.core.annotation.Api;
+import org.jsondoc.core.annotation.ApiMethod;
+import org.jsondoc.core.annotation.ApiPathParam;
+import org.jsondoc.core.pojo.ApiStage;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -9,6 +13,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping(value = "/booking")
+@Api(name = "Hotel Booking API", description = "", stage = ApiStage.RC)
 public class HotelControllerRepoSample {
 
     @Autowired
@@ -19,23 +24,27 @@ public class HotelControllerRepoSample {
     }*/
 
     @RequestMapping(value = "/alls", method = RequestMethod.GET)
+    @ApiMethod(description = "all booking")
     public List<HotelBooking> getBookings() {
         return bookingRepository.findAll();
     }
 
     @RequestMapping(value = "/affordables/{price}")
-    public List<HotelBooking> getAffordableBooking(@PathVariable double price) {
+    @ApiMethod(description = "affordable booking")
+    public List<HotelBooking> getAffordableBooking(@ApiPathParam(name = "price") @PathVariable double price) {
         return bookingRepository.findByPriceOfNigthLessThanEqual(price);
     }
 
     @RequestMapping(value = "/creates", method = RequestMethod.POST)
+    @ApiMethod(description = "create booking")
     public List<HotelBooking> createBooking(@RequestBody HotelBooking hotelBooking) {
         bookingRepository.save(hotelBooking);
         return bookingRepository.findAll();
     }
 
     @RequestMapping(value = "/remove/{id}", method = RequestMethod.DELETE)
-    public List<HotelBooking> deleteBooking(@PathVariable long id) {
+    @ApiMethod(description = "delete booking")
+    public List<HotelBooking> deleteBooking(@ApiPathParam(name = "id")@PathVariable long id) {
         bookingRepository.delete(id);
         return bookingRepository.findAll();
     }
